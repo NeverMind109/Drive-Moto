b11c981e2de00e5fdebd4533b8a876a6359a1460
NeverMind109 <vladgladkih123@gmail.com>
2022-04-06 21:27:07 +0300

    Created: Added Search-Tabs Section

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,12 @@ $(function () {
     nextArrow:
       '<button class="banner-section__slider-button banner-section__slider-next" type="button"><img src="images/banner-slider-arrow-next.svg" alt="Next Arrow" class="banner-section__slider-next-img"></button>',
   });
+
+  $(".search__tabs-item").on("click", function (e) {
+    e.preventDefault();
+    $(".search__tabs-item").removeClass("search__tabs-item--active");
+    $(".search__content-item").removeClass("search__content-item--active");
+    $(this).addClass("search__tabs-item--active");
+    $($(this).attr("href")).addClass("search__content-item--active");
+  });
 });
