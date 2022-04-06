03ee3733d9705dbff4e02051bc759904d1878e0a
NeverMind109 <vladgladkih123@gmail.com>
2022-04-06 19:10:14 +0300

    Created: Added Banner Section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
-    
-})
+  $(".banner-section__slider").slick({
+    dots: true,
+    prevArrow:
+      '<button class="banner-section__slider-button banner-section__slider-prev" type="button"><img src="images/banner-slider-arrow-prev.svg" alt="Previous Arrow" class="banner-section__slider-prev-img"></button>',
+    nextArrow:
+      '<button class="banner-section__slider-button banner-section__slider-next" type="button"><img src="images/banner-slider-arrow-next.svg" alt="Next Arrow" class="banner-section__slider-next-img"></button>',
+  });
+});
